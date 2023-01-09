0bef566bae8ea716ae5a781982d9dd3019f8d703
Butter on Bread <qorbaxk97@gmail.com>
2023-01-09 17:40:29 +0900

    promise await 연습

---
+++ question/promise.js
@@ -0,0 +1,44 @@
+// function delay(ms){
+//   return new Promise(resolve=>setTimeout(resolve,ms))
+// }
+// delay(3000).then(()=>console.log('3초후실행'))
+
+//await 을 어디에 붙여야할까?
+async function f() {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("완료!"), 1000);
+  });
+
+  let result = await promise;
+
+  console.log(result); // "완료!"
+}
+
+f();
+
+// then을 await으로 고치기
+// function loadJson(url) {
+//   return fetch(url)
+//     .then(response => {
+//       if (response.status == 200) {
+//         return response.json();
+//       } else {
+//         throw new Error(response.status);
+//       }
+//     })
+// }
+
+// loadJson('no-such-user.json')
+//   .catch(alert); // Error: 404
+
+async function loadJson(url) {
+  let response = await fetch(url);
+
+  if (response.status == 200) {
+    let json = await response.json();
+    return json;
+  }
+  throw new Error(response.status);
+}
+
+loadJson("no-such-user.json").catch(console.log); // Error: 404
